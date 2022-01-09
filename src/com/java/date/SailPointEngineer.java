5ca864052d87a1965c5899ee96f64a918ba356ab
ramir <ramir@DESKTOP-H3USA89>
2022-01-09 17:04:00 +0530

    commit

---
+++ src/com/java/date/SailPointEngineer.java
@@ -0,0 +1,12 @@
+package com.java.date;
+package com.java.util.*;
+
+public class SailPointEngineer {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+   String ass = "Assosiative";
+   
+	}
+
+}
---
+++ src/com/java/date/Test123.java
@@ -0,0 +1,10 @@
+package com.java.date;
+
+public class Test123 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
